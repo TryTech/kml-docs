084660e80145beba4a0fe478aaff15cb6aa2015f
ItaloCobains <italobrandao.contato@gmail.com>
2024-05-24 09:57:18 -0300

    chore: Add remark-mdx-disable-explicit-jsx plugin for MDX options in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+import remarkMdxDisableExplicitJsx from 'remark-mdx-disable-explicit-jsx'
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  latex: true,
+  mdxOptions: {
+    remarkPlugins: [
+      [
+        remarkMdxDisableExplicitJsx,
+        { whiteList: ['table', 'thead', 'tbody', 'tr', 'th', 'td'] }
+      ]
+    ]
+  }
 })
 
 module.exports = withNextra()
